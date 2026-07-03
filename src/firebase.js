1782b0ff9df082a442f5d79d975a4bba3c7ad86a
rotnotalukder266-debug <rotnotalukder266@gmail.com>
2026-07-03 21:36:16 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDqgvFr_leQ5roh7aBatx8-gnGftmGLq2M",
   authDomain: "earntrack-8ca5a.firebaseapp.com",
@@ -6,5 +9,8 @@ const firebaseConfig = {
   storageBucket: "earntrack-8ca5a.firebasestorage.app",
   messagingSenderId: "681234743388",
   appId: "1:681234743388:web:af2ec6051286c448912699",
-  measurementId: "G-4WK6R7Y9RK"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
