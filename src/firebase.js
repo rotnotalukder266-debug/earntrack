331fdffaff1c7d34af87a13d70f373b5ef8bf8b6
rotnotalukder266-debug <rotnotalukder266@gmail.com>
2026-07-19 11:33:14 +0600

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// এখানে আপনার Firebase কনসোল থেকে পাওয়া কনফিগ বসান
+// Firebase Console > Project Settings > General > Your apps > SDK setup and configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqgvFr_leQ5roh7aBatx8-gnGftmGLq2M",
+  authDomain: "earntrack-8ca5a.firebaseapp.com",
+  projectId: "earntrack-8ca5a",
+  storageBucket: "earntrack-8ca5a.firebasestorage.app",
+  messagingSenderId: "681234743388",
+  appId: "1:681234743388:web:af2ec6051286c448912699",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
